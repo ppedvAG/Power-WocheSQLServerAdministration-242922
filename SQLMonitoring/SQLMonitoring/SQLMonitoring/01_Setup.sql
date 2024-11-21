5827c7a94ce97ff9c88aed58093d6bd7261f4a91
Rauchendraser <andreasr@ppedv.de>
2024-11-21 15:03:02 +0100

    Setup Partitionierung

---
+++ SQLMonitoring/SQLMonitoring/SQLMonitoring/01_Setup.sql
@@ -0,0 +1,45 @@
+/*
+
+
+Instanzen aus Performancegründen?
+
+Einstellungen des Servers
+
+
+
+where spalte like '%Haus%'
+
+
+'maier'
+
+*/
+select soundex('meyr')
+
+
+--Volumewartungstask
+
+8 64  64 64 64 64 64
+---------------------------
+11111111111111111111111111
+--------------------------
+
+
+--Trenne DAten von Logfiles physikalisch
+
+L: Logfile
+M: Daten
+
+
+create database testdb
+
+--Tempdb
+
+-- #tab  ##tab
+--Auslagerungen
+--IX Wartung
+
+--sollten eig HDDs haben
+
+
+create table txy (id int, spx char(4100), spy varchar(4100)) 
+--Trenne daten von Logs
